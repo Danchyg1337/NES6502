4822f96951140e6e8c42821fecef545b3cdde8ce
Your Name <you@example.com>
2021-03-15 23:24:13 +0300

    guiv0.1

--- NES6502.cpp
+++ NES6502.cpp
@@ -2,16 +2,18 @@
 #include <fstream>
 #include <vector>
 #include <unordered_map>
-
+#include "Defines.h"
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
 class CPU {
 public:
-    CPU(uint8_t* program, size_t size) {
-        Reset();
+    CPU() {}
+
+    void load (uint8_t* program, size_t size) {
+        Reset ();
         memory = new uint8_t[2048 * 32];
-        memcpy(&memory[PC], program, size);
+        memcpy (&memory[PC], program, size);
     }
 
     uint8_t* memory;
@@ -43,8 +45,11 @@ public:
         SR = FLAGS::IGNORED;
     }
 
+
     uint8_t Fetch() {
         uint8_t fetched = memory[PC];
+
+
         PC++;
         return fetched;
     }
@@ -185,23 +190,14 @@ public:
     }
 
     void Show() {
-        #define BYTE_TO_BINARY(byte)  \
-        (byte & 0x80 ? '1' : '0'), \
-        (byte & 0x40 ? '1' : '0'), \
-        (byte & 0x20 ? '1' : '0'), \
-        (byte & 0x10 ? '1' : '0'), \
-        (byte & 0x08 ? '1' : '0'), \
-        (byte & 0x04 ? '1' : '0'), \
-        (byte & 0x02 ? '1' : '0'), \
-        (byte & 0x01 ? '1' : '0')
-
         //system("cls");
-        printf("SR %c%c%c%c%c%c%c%c\n", BYTE_TO_BINARY(this->SR));
-        printf("PC %04X\n", this->PC);
-        printf("SP %04X\n", this->SP);
-        printf("A %02X\n", this->A);
-        printf("X %02X\n", this->X);
-        printf("Y %02X\n", this->Y);
+
+        printf ("SR %c%c%c%c%c%c%c%c\n", BYTE_TO_BINARY (this->SR));
+        printf ("PC %04X\n", this->PC);
+        printf ("SP %04X\n", this->SP);
+        printf ("A %02X\n", this->A);
+        printf ("X %02X\n", this->X);
+        printf ("Y %02X\n", this->Y); 
 
     }
 
@@ -226,32 +222,56 @@ private:
 
 class NES : public olc::PixelGameEngine
 {
+    CPU CPU6502;
+
 public:
     NES()
     {
         sAppName = "NES";
     }
-
-
+    
 
 public:
     bool OnUserCreate() override
     {
         std::ifstream file;
-        file.open("test1.6502", std::ifstream::binary);
+        std::string file_name = "test1.6502";
+        file.open(file_name, std::ifstream::binary);
+        if (!file.is_open()) {
+            std::cout << "No input file" << file_name << std::endl;
+        }
         std::vector<uint8_t> program(std::istreambuf_iterator<char>(file), {});
-        CPU CPU6502(&program[0], program.size());
+        CPU6502.load(program.data(), program.size());
         CPU6502.Run();
         return true;
+    }
+
+    template<class... Ts> 
+    void print_parametr (int x, int y, std::string buff, const char* text, Ts&&... args) {
+        sprintf_s (buff.data (), buff.size (), text, args...);
+        DrawString (x, y, buff, olc::RED, 1);
     }
 
     bool OnUserUpdate(float fElapsedTime) override
     {
         Clear(olc::Pixel(255, 128, 255));
-        /*for (int x = 0; x < ScreenWidth(); x++)
-            for (int y = 0; y < ScreenHeight(); y++)
-                Draw(x, y, olc::Pixel(rand() % 255, rand() % 255, rand() % 255));*/
-        DrawString(ScreenWidth() - 50, ScreenHeight() - 10, "HELLO");
+       
+        int nes_width = 256;
+        int nes_height = 240;
+
+        for (int x = 0; x < nes_width; x++)
+            for (int y = 0; y < nes_height; y++)
+                Draw(x, y, olc::Pixel(rand() % 255, rand() % 255, rand() % 255));
+
+        std::string a (50, ' ');
+
+        print_parametr (nes_width + 5, 10, a, "Accamulator %02X", CPU6502.A);
+        print_parametr (nes_width + 5, 20, a, "X Register %02X", CPU6502.X);
+        print_parametr (nes_width + 5, 30, a, "Y Register %02X", CPU6502.Y);
+        print_parametr (nes_width + 5, 40, a, "Stack Pointer %04X", CPU6502.SP);
+        print_parametr (nes_width + 5, 50, a, "Program Counter %04X", CPU6502.PC);
+        print_parametr (nes_width + 5, 60, a, "Status Register %c%c%c%c%c%c%c%c", BYTE_TO_BINARY(CPU6502.SR));
+
         return true;
     }
 };
@@ -260,6 +280,6 @@ public:
 int main()
 {
     NES nes;
-    if (nes.Construct(256, 240, 4, 4))
+    if (nes.Construct(256 + 200, 240, 4, 4))
         nes.Start();
 }
